a1e850da97f08a9bca98fb721e48eed4a8554e01
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-12-04 11:49:41 +0300

    added search functionality

--- chats/views.py
+++ chats/views.py
@@ -122,6 +122,17 @@ def single_image(request, image_id):
     return render(request, 'image.html', {'image':image, 'form':form, 'comments':comments})
 
 
+def search(request):
+    if 'search' in request.GET and request.GET['search']:
+        search_term = request.GET.get('search')
+        profiles = Profile.search_profile(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html',{'message':message, 'profiles':profiles})
+    else:
+        message = 'Enter term to search'
+        return render(request, 'search.html', {'message':message})
+
 
 
 
