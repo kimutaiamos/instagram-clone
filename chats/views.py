e586a4f262b8b40b451496cfd6950cd9d36dc09d
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-12-04 11:45:32 +0300

    added images upload

--- chats/views.py
+++ chats/views.py
@@ -73,5 +73,24 @@ def profile(request, username):
 
 
 
+@login_required(login_url='/accounts/login')
+def upload_image(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            upload = form.save(commit=False)
+            upload.profile = request.user
+            # print(f'image is {upload.image}')
+            upload.save()
+            return redirect('profile', username=request.user)
+    else:
+        form = ImageForm()
+    
+    return render(request, 'profile/upload_image.html', {'form':form})
+
+
+
+
+
 
 
