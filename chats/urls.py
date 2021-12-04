7775846281f8737299eeea4d4ef30f7c0ee80753
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-12-04 12:08:55 +0300

    added templates

--- chats/urls.py
+++ chats/urls.py
@@ -13,3 +13,6 @@ urlpatterns=[
     url(r'^image/(?P<image_id>\d+)', views.single_image, name='single_image'),
     url(r'^search/', views.search, name='search')
 ]
+
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
